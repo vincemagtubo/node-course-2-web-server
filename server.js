99824c2ab8a37b63cbc84387aba40f8ca69c5382
vincemagtubo <johnvincemagtubo@gmail.com>
2017-05-25 19:39:15 +0800

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -63,6 +65,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('It runnin fucka');
+app.listen(port, () => {
+    console.log(`It runnin fucka on port ${port}`);
 });
